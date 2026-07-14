9cc55ea69c2d3e84fa00198e03c750f80f89ddd2
taneracar <taneracar1@icloud.com>
2026-07-13 17:47:26 -0700

    feat: implement user retrieval endpoints in UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  //Get /user
+
+  @Get()
+  GetUsers(@Query('name') name: string) {
+    const users = [
+      { id: 1, name: 'Jenna' },
+      { id: 2, name: 'Dookie' },
+      { id: 3, name: 'Taner' },
+    ];
+
+    if (!name) {
+      return users;
+    }
+
+    return users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase()),
+    );
+  }
+  //Get /user/:id
+  @Get(':id')
+  GetUser(id: number) {
+    return { id: id, name: 'Taner' };
+  }
+}
